3fa18f89da617cc48fc6491edf447c8a20cb83be
McClane Howland <mcclane.howland@gmail.com>
2020-04-05 14:39:41 -0700

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const WebSocket = require('ws')
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
-port = 80
+port = 8080
 
 
 app.use(express.static('public'))
